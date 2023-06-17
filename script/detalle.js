9a72d87ea949b04a04704f8d9cc09f1d01323a21
Pia Lopez <ppialopez@gmail.com>
2023-06-17 20:35:47 -0300

    agregue saltos de linea a la pagina product

--- script/detalle.js
+++ script/detalle.js
@@ -14,6 +14,7 @@ let item = `
   <p class="pt-3">$${product.price}</p> <hr>
   <p class="pt-3">ID:${product.id}</p> <hr>
   <p class="pt-3 text-center">${product.decription}</p>
+  <p class="pt-3 text-center">${product.stock}</p>
   <button class="agregar-carrito my-3 w-75  btn btn-danger border border-black" id="${product.name}" onclick="addCard()">Agregar al Carrito</button> 
 `;
 card.innerHTML = item;
@@ -65,35 +66,6 @@ window.onload = () => {
   }
 };
 
-// abrir el modal al hacer clic en "Agregar al Carrito"
+// asbrir el modal al hacer clic en "Agregar al Carrito"
 let verCarrito = document.querySelector(".agregar-carrito");
 verCarrito.addEventListener("click", showCar);
-
-
-
-
-
-
-/* //------------------------------PARA AGREGAR CARRITO CON BOTON-------------------------------
-
-const frutsSection = document.querySelector('#fruts');
-let countCard = 0 //esto es para hacer un contador que este en 0
-
-function addCard(event) {
-    let itemCards = document.querySelector('#card') //con esto agarro el 0 que esta en el HTML
-    countCard = countCard + 1 //con esto aumento el numero de mi cero
-    itemCards.innerText = countCard //ahi estoy agarrando el 0 con item card, cambiando el texto con inner text y diciendo que es igual a count card para que se vaya sumando
-}
-
-fruts.forEach(function (frut) {
-    let card = document.createElement('div')
-    let item = `
-    <p>${frut.url}</p>
-    <p>${frut.name}</p>
-    <p>${frut.price}</p>
-    <button id=${frut.name} onclick="addCard(event)" >Comprar</button> 
-    `; //en lo que seria el button es donde agrego el evento que se llama on click y le agrego la funcion que cree arriba y le agrego parentisis para inicializar la funcion
-
-    card.innerHTML = item
-    frutsSection.appendChild(card)
-}) */
