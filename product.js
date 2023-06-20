964c33431dbfe2c3b5d4e054f1a240e714bf6b49
Pia Lopez <ppialopez@gmail.com>
2023-06-20 15:48:21 -0300

    se arreglo rutas

--- product.js
+++ product.js
@@ -2,10 +2,10 @@ let galeryarray = [
   {
       id: 1236,
       name: `Remera Under Armour Live GP`,
-      src: "assets/images/195252511982-1.jpeg",
-      description: `- Esta mezcla de algodón super suave, te brinda comodidad durante todo el dia
-      - Cuello acanalado
-      - 60% Algodon/40% Polyester
+      src: "../assets/images/195252511982-1.jpeg",
+      description: `- Esta mezcla de algodón super suave, te brinda comodidad durante todo el dia <br>
+      - Cuello acanalado <br>
+      - 60% Algodon/40% Polyester <br>
       - Ideal para entrenar`,
       price: 11.499,
       stock: 'Stock Disponible',
@@ -15,7 +15,7 @@ let galeryarray = [
   {
     id: 3256,
     name: `Short Project Rock Fleece para mujer`,
-    src:"assets/images/img-short.jpeg" ,
+    src:"../assets/images/img-short.jpeg" ,
     description: `- Tejido Fleece de mezcla de algodón ligero y ultra cómodo
     - Suave capa interior que mantiene la calidez y la comodidad
     - Amplia cintura elástica con revestimiento y cordón exterior
@@ -27,7 +27,7 @@ let galeryarray = [
   {
       id:6589 ,
       name: `Calzas UA RUSH™ No-Slip Waistband Full-Length para mujer`,
-      src: "assets/images/calza.jpeg",
+      src: "../assets/images/calza.jpeg",
       description: ` - UA RUSH™ es la tecnología infrarroja que refleja la energía del cuerpo para rendir más y recuperarte más rápido
       - Material que capilariza el sudor y se seca rápidamente
       - Ubicación avanzada de las costuras para eliminar los puntos de presión y los roces
@@ -40,7 +40,7 @@ let galeryarray = [
     {
       id:2365,
       name: `Pantalón corto UA Rival Fleece Graphic para Hombre`,
-      src:"assets/images/shotm1.jpeg" ,
+      src:"../assets/images/shotm1.jpeg" ,
       description: `- Tejido Fleece de mezcla de algodón ligero y ultracómodo con interior cepillado para    mantenerte abrigado y cómodo
       - Cintura elástica recubierta con cordón de ajuste externo
       - Bolsillos abiertos para las manos y bolsillo trasero seguro con cierre a presión`,
@@ -52,7 +52,7 @@ let galeryarray = [
     {
       id: 9856,
       name: `Remera UA RUSH™ Energy para hombre`,
-      src:"assets/images/rem.jpeg" ,
+      src:"../assets/images/rem.jpeg" ,
       description: `- Tejido Fleece de mezcla de algodón ligero y ultra cómodo
       - Suave capa interior que mantiene la calidez y la comodidad
       - Amplia cintura elástica con revestimiento y cordón exterior
@@ -64,7 +64,7 @@ let galeryarray = [
     {
       id: 8649 ,
       name: `Camiseta Titular Argentina 3 Estrellas 2022`,
-      src: "assets/images/arg.jpg",
+      src: "../assets/images/arg.jpg",
       description:` - UA RUSH™ es la tecnología infrarroja que refleja la energía del cuerpo para rendir más y recuperarte más rápido
       - UA RUSH™ es una tecnología infrarroja que refleja la energía de tu cuerpo para ayudarte a trabajar más duro y recuperarte más rápido
       - El material elástico en 4 direcciones se mueve mejor en todas las direcciones
@@ -76,7 +76,7 @@ let galeryarray = [
     {
       id: 5698,
       name: `Remera de entrenamiento Under Armour Sportstyle Logo para niños`,
-      src:"assets/images/remchi.jpeg" ,
+      src:"../assets/images/remchi.jpeg" ,
       description: `- Esta mezcla de algodón super suave, te brinda comodidad durante todo el dia
       - Cuello acanalado
       - 60% Algodon/40% Polyester`,
@@ -87,7 +87,7 @@ let galeryarray = [
     {
       id: 9695,
       name:`Remera manga corta UA Multilogo Short Sleeve para niño` ,
-      src: "assets/images/remchil2.jpeg",
+      src: "../assets/images/remchil2.jpeg",
       description: `- Tejido supersuave de mezcla de algodón que proporciona comodidad durante todo el día.
       - Cuello acanaladoTejido supersuave de mezcla de algodón que proporciona comodidad durante todo el día.
       - Cuello acanalado`,
@@ -98,7 +98,7 @@ let galeryarray = [
     {
       id:2365 ,
       name:`Remera UA BASKETBALL LOCK UP para niños` ,
-      src:"assets/images/remchil3.jpeg" ,
+      src:"../assets/images/remchil3.jpeg" ,
       description: ` - Tejido supersuave de mezcla de algodón que proporciona comodidad durante todo el día.`,
       price:12.897,
       stock: 'Stock Disponible',  
