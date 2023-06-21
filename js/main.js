e0ea44f30f2aedd599b6505e02e651417e2e4437
faridaleali <faleali.contact@gmail.com>
2023-06-20 22:36:57 -0300

    se arreglo la etiqueta a para redirigir los productos a detalle

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ for (var i= 0; i< curarray.length; i++) {
     <div class="col-lg-4 text-center mb-2 mt-5">
           <div class="card border-0 bg-light">
             <div class="card-body">
-              <a id="abrirProducto" href="./pages/detalles.html">
+              <a id="abrirProducto" href="./pages/detalle.html">
                 <img src="${curarray[i].src}"  class="img-fluid object-fit-cover h-100 w-100" alt="${curarray[i].id}">
               </a>
             </div>
